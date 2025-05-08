5a84c063d3be7b12a65624d34eb7dff97dd96fe0
ikeyu0806 <ikeyu0806@gmail.com>
2025-05-08 14:42:34 +0900

    implement CandidatesInsights component

--- app/use_context/page.tsx
+++ app/use_context/page.tsx
@@ -1,14 +1,25 @@
 import CreatorLists from "@/components/UseContextDemo/CreatorLists"
 import CandidatesLists from "@/components/UseContextDemo/CandidatesLists"
+import CandidatesInsights from "@/components/UseContextDemo/CandidatesInsights"
 
 export default function UseContextPage() {
   return (
     <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-12 my-4 p-4">
+        <h1 className="text-2xl font-bold">UseContext Demo</h1>
+        <p className="text-gray-600">
+          UseContextを使ったコンポーネントのデモです。<br />
+          クリエイターを選定するためのコンポーネントです。
+        </p>
+      </div>
       <div className="col-span-4 my-4 p-4">
         <CreatorLists />
       </div>
       <div className="col-span-4 my-4 p-4">
         <CandidatesLists />
+      </div>
+      <div className="col-span-4 my-4 p-4">
+        <CandidatesInsights />
       </div>
     </div>
   )
---
+++ components/UseContextDemo/CandidatesInsights.tsx
@@ -0,0 +1,11 @@
+export default function CandidatesInsights() {
+  return (
+    <div className="my-4 p-4 bg-white border border-gray-200 rounded-lg shadow-md">
+      <div>合計フォロワー数: 1000人</div>
+      <div>合計いいね数: 10000</div>
+      <button className="bg-blue-500 text-white rounded-lg px-4 py-2 mt-4">
+        選定を確定する
+      </button>
+    </div>
+  )
+}
