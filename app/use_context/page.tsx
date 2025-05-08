7a512de2446be42f952bb4eb8c7131f09a119fa6
ikeyu0806 <ikeyu0806@gmail.com>
2025-05-08 14:17:17 +0900

    implement useContext CandidatesLists component

--- app/use_context/page.tsx
+++ app/use_context/page.tsx
@@ -1,10 +1,14 @@
 import CreatorLists from "@/components/UseContextDemo/CreatorLists"
+import CandidatesLists from "@/components/UseContextDemo/CandidatesLists"
 
 export default function UseContextPage() {
   return (
     <div className="grid grid-cols-12 gap-4">
-      <div className="col-span-4">
+      <div className="col-span-4 my-4 p-4">
         <CreatorLists />
+      </div>
+      <div className="col-span-4 my-4 p-4">
+        <CandidatesLists />
       </div>
     </div>
   )
---
+++ components/UseContextDemo/CandidatesLists.tsx
@@ -0,0 +1,14 @@
+import CreatorCard from "./CreatorCard";
+
+export default function CandidatesLists() {
+  return (
+    <div className="my-4">
+      <CreatorCard />
+      <CreatorCard />
+      <CreatorCard />
+      <CreatorCard />
+      <CreatorCard />
+      <CreatorCard />
+    </div>
+  );
+}
