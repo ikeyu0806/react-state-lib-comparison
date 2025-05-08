4acf8390ee343c9f91d2b88c642964098df29315
ikeyu0806 <ikeyu0806@gmail.com>
2025-05-08 17:12:35 +0900

    implement useContext CreatorLists component

--- components/UseContextDemo/CreatorLists.tsx
+++ components/UseContextDemo/CreatorLists.tsx
@@ -1,21 +1,38 @@
 import React, { useContext } from 'react'
-import { UnselectedCreatorContext } from '@/app/use_context/page'
-import { Creator } from '@/types/creator'
+import {
+  SelectedCreatorContext,
+  UnselectedCreatorContext,
+} from '@/app/use_context/page'
 import CreatorCard from './CreatorCard'
+import { Creator } from '@/types/creator'
 
 export default function CreatorLists() {
-  const [UnselectedCreators] = useContext(UnselectedCreatorContext)
+  const [selectedCreators, setSelectedCreators] = useContext(
+    SelectedCreatorContext,
+  )
+  const [unselectedCreators, setUnselectedCreators] = useContext(
+    UnselectedCreatorContext,
+  )
+
+  const handleClick = (creator: Creator) => {
+    const newSelectedCreators = [...selectedCreators, creator]
+
+    const newUnselectedCreators = unselectedCreators.filter(
+      (c) => c.name !== creator.name,
+    )
+    setSelectedCreators(newSelectedCreators)
+    setUnselectedCreators(newUnselectedCreators)
+  }
+
   return (
     <div className="my-4">
-      {UnselectedCreators.map((creator) => (
+      {unselectedCreators.map((creator) => (
         <CreatorCard
           key={creator.name}
           name={creator.name}
           followers={creator.followers}
           totalLikes={creator.totalLikes}
-          onClick={() => (event: React.MouseEvent) => {
-            console.log(event)
-          }}
+          onClick={() => handleClick(creator)}
           isSelected={false}
         />
       ))}
