04fd56f7bde57c400e6b0fe3ad40b04abf1cc3b9
ikeyu0806 <ikeyu0806@gmail.com>
2025-05-08 17:35:21 +0900

    implement CandidatesInsights.tsx

--- components/UseContextDemo/CandidatesInsights.tsx
+++ components/UseContextDemo/CandidatesInsights.tsx
@@ -1,8 +1,21 @@
+import React, { useContext } from 'react'
+import {
+  SelectedCreatorContext
+} from '@/app/use_context/page'
+
 export default function CandidatesInsights() {
+    const [selectedCreators, _] = useContext(
+      SelectedCreatorContext,
+    )
+  
   return (
     <div className="my-4 p-4 bg-white border border-gray-200 rounded-lg shadow-md">
-      <div>合計フォロワー数: 1000人</div>
-      <div>合計いいね数: 10000</div>
+      <div>合計フォロワー数: {selectedCreators.reduce((accumulator, creator) => {
+        return accumulator + creator.followers
+      }, 0)}人</div>
+      <div>合計いいね数: {selectedCreators.reduce((accumulator, creator) => {
+        return accumulator + creator.totalLikes
+      }, 0)}</div>
       <button className="bg-blue-500 text-white rounded-lg px-4 py-2 mt-4">
         選定を確定する
       </button>
