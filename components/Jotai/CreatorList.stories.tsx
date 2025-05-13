9b236d69ea7b77e433203ac47766b12a92d614eb
ikeyu0806 <ikeyu0806@gmail.com>
2025-05-13 19:03:11 +0900

    add Jotai CreatorList storybook

--- components/Jotai/CreatorList.stories.tsx
+++ components/Jotai/CreatorList.stories.tsx
@@ -1,51 +1,56 @@
+// CreatorLists.stories.tsx
+
 import React, { ReactNode } from 'react'
 import { StoryObj, Meta } from '@storybook/react'
 import CreatorLists from './CreatorLists'
-import { SelectedCreatorContext, UnselectedCreatorContext } from '@/app/use_context/page'
+import { useAtom, useSetAtom } from 'jotai'
+import {
+  selectedCreatorsAtom,
+  unselectedCreatorsAtom,
+} from '@/store/jotai/creator_atoms'
 import { Creator } from '@/types/creator'
 
 const meta = {
   component: CreatorLists,
-  parameters: {},
-  tags: ['autodocs'],
-  argTypes: {},
+    parameters: {},
+    tags: ['autodocs'],
+    argTypes: {}
 } satisfies Meta<typeof CreatorLists>
 
 type Story = StoryObj<typeof meta>
 
-const mockUnselectedCreators: Creator[] = [
+// モックデータを作成
+const mockUnselectedCreators = [
   {
-    name: 'クリエイター',
+    name: 'Creator One',
     followers: 1000,
     totalLikes: 5000,
   },
   {
-    name: 'クリエイター2',
+    name: 'Creator Two',
     followers: 2000,
     totalLikes: 8000,
   },
   {
-    name: 'クリエイター3',
+    name: 'Creator Three',
     followers: 1500,
     totalLikes: 6000,
   },
 ]
 
-const mockDispatchSelected = () => {}
-const mockDispatchUnselected = () => {}
-
+// Storybook 用のラッパーコンポーネント
 const Wrapper = ({ children }: { children: ReactNode }) => {
-  return (
-    <SelectedCreatorContext.Provider value={[[], mockDispatchSelected]}>
-      <UnselectedCreatorContext.Provider value={[mockUnselectedCreators, mockDispatchUnselected]}>
-        {children}
-      </UnselectedCreatorContext.Provider>
-    </SelectedCreatorContext.Provider>
+  // Jotai ストアのモック
+  const [_, setUnselectedCreators] = useAtom(
+    unselectedCreatorsAtom,
   )
+  setUnselectedCreators(mockUnselectedCreators)
+
+  return <div>{children}</div>
 }
 
 export default {
-  title: 'Components/UseContext/CreatorLists',
+  title: 'Components/Jotai/CreatorLists',
   component: CreatorLists,
   decorators: [
     (Story: React.FC) => (
