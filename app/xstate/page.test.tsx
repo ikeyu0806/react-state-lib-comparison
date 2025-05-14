0dcc7648723a8f82d8b4db77cc212aa570a4b422
ikeyu0806 <ikeyu0806@gmail.com>
2025-05-14 16:14:26 +0900

    add xstate test

--- app/xstate/page.test.tsx
+++ app/xstate/page.test.tsx
@@ -1,17 +1,43 @@
 import { render, screen } from '@testing-library/react'
-import { describe, test, expect, vi } from 'vitest'
+import { describe, test, expect, vi, beforeEach } from 'vitest'
 import RecruitmentStatusComponent from './page'
 
-vi.mock('@xstate/react', () => {
-  return {
-    useMachine: () => [{ context: { status: 'unpublished' } }, vi.fn()],
-  }
-})
+// モジュールを先頭でモック化
+vi.mock('@xstate/react')
+
+// モックされたモジュールをモック化の後にインポート
+import { useMachine } from '@xstate/react'
 
 describe('RecruitmentStatusComponent', () => {
-  test('should render the current status', () => {
-    render(<RecruitmentStatusComponent />)
+  // 各テスト前にモックをリセット
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
 
+  test('should render the current status', () => {
+    // モックを設定
+    vi.mocked(useMachine).mockReturnValue([
+      // 以下の警告が出るが動作には影響せず対応辛いのでignore
+      // 型 '{ context: { status: string; }; }' を型 'MachineSnapshot<any, any, any, any, any, any, any, any>' に割り当てることはできません。
+      // 型 '{ context: { status: string; }; }' には 型 'StoppedMachineSnapshot<any, any, any, any, any, any, any, any>' からの次のプロパティがありません: status, output, error, machine、10 など。
+      // @ts-ignore
+      { context: { status: 'unpublished' } },
+      vi.fn(),
+    ])
+
+    render(<RecruitmentStatusComponent />)
     expect(screen.getByText('Current Status: unpublished')).toBeInTheDocument()
+  })
+
+  test('should render the current status after state change', () => {
+    // モックを設定
+    vi.mocked(useMachine).mockReturnValue([
+      // @ts-ignore
+      { context: { status: 'recruiting' } },
+      vi.fn(),
+    ])
+
+    render(<RecruitmentStatusComponent />)
+    expect(screen.getByText('Current Status: recruiting')).toBeInTheDocument()
   })
 })
